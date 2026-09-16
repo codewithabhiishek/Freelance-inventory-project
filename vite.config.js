466dcbef293d12066ea18de3260d0f9acd3bb4c1
codewithabhiishek <socials.abhiishek@gmail.com>
2026-09-16 16:10:14 +0530

    fix(vite): configure dynamic host and non-conflicting port for local development

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      port: 3000,
-    },
+    host: true,
+    port: 3001,
   },
 });
